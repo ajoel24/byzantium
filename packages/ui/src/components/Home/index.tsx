e96c45e4b1625625f00ca8401e8f184745cb9b98
ajoel24 <drew.devops@gmail.com>
2022-02-10 21:46:57 +0530

    Add rsuite.js
    - Using React Suite for UI components.

---
+++ packages/ui/src/components/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'rsuite';
+
+function Home() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default Home;
